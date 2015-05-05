3e4cae3e42133fd7230647f77c93b29862d49f6b
mangochiman <mangochiman@gmail.com>
2015-05-05 11:47:48 +0200

    Added a news banner in the application.rhtml file

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,49 +10,119 @@ class AdminController < ApplicationController
     uri = "http://www.who.int/feeds/entity/csr/don/en/rss.xml"
     output = RestClient.get(uri)
     doc = Nokogiri::XML(output)
-=begin
     @xml = doc.xpath("//channel/item").map do |i|
       {
         'title' => i.xpath('title').text, 'description' => i.xpath('description').text
       }
     end
-    #raise @xml.inspect
-=end
+    @headlines = @xml.map{|x|x["title"]}.join("<span class='separator'>|</span>")
   end
 
   def regional_dashboard
-    
+    uri = "http://www.who.int/feeds/entity/csr/don/en/rss.xml"
+    output = RestClient.get(uri)
+    doc = Nokogiri::XML(output)
+    @xml = doc.xpath("//channel/item").map do |i|
+      {
+        'title' => i.xpath('title').text, 'description' => i.xpath('description').text
+      }
+    end
+    @headlines = @xml.map{|x|x["title"]}.join("<span class='separator'>|</span>")
   end
 
   def district_dashboard
-    
+    uri = "http://www.who.int/feeds/entity/csr/don/en/rss.xml"
+    output = RestClient.get(uri)
+    doc = Nokogiri::XML(output)
+    @xml = doc.xpath("//channel/item").map do |i|
+      {
+        'title' => i.xpath('title').text, 'description' => i.xpath('description').text
+      }
+    end
+    @headlines = @xml.map{|x|x["title"]}.join("<span class='separator'>|</span>")
   end
 
   def northern_zone
-    
+    uri = "http://www.who.int/feeds/entity/csr/don/en/rss.xml"
+    output = RestClient.get(uri)
+    doc = Nokogiri::XML(output)
+    @xml = doc.xpath("//channel/item").map do |i|
+      {
+        'title' => i.xpath('title').text, 'description' => i.xpath('description').text
+      }
+    end
+    @headlines = @xml.map{|x|x["title"]}.join("<span class='separator'>|</span>")
   end
 
   def central_zone
-    
+    uri = "http://www.who.int/feeds/entity/csr/don/en/rss.xml"
+    output = RestClient.get(uri)
+    doc = Nokogiri::XML(output)
+    @xml = doc.xpath("//channel/item").map do |i|
+      {
+        'title' => i.xpath('title').text, 'description' => i.xpath('description').text
+      }
+    end
+    @headlines = @xml.map{|x|x["title"]}.join("<span class='separator'>|</span>")
   end
 
   def southern_zone
-
+    uri = "http://www.who.int/feeds/entity/csr/don/en/rss.xml"
+    output = RestClient.get(uri)
+    doc = Nokogiri::XML(output)
+    @xml = doc.xpath("//channel/item").map do |i|
+      {
+        'title' => i.xpath('title').text, 'description' => i.xpath('description').text
+      }
+    end
+    @headlines = @xml.map{|x|x["title"]}.join("<span class='separator'>|</span>")
   end
 
   def diagnosis_settings_menu
-    
+    uri = "http://www.who.int/feeds/entity/csr/don/en/rss.xml"
+    output = RestClient.get(uri)
+    doc = Nokogiri::XML(output)
+    @xml = doc.xpath("//channel/item").map do |i|
+      {
+        'title' => i.xpath('title').text, 'description' => i.xpath('description').text
+      }
+    end
+    @headlines = @xml.map{|x|x["title"]}.join("<span class='separator'>|</span>")
   end
 
   def add_diagnosis
-    
+    uri = "http://www.who.int/feeds/entity/csr/don/en/rss.xml"
+    output = RestClient.get(uri)
+    doc = Nokogiri::XML(output)
+    @xml = doc.xpath("//channel/item").map do |i|
+      {
+        'title' => i.xpath('title').text, 'description' => i.xpath('description').text
+      }
+    end
+    @headlines = @xml.map{|x|x["title"]}.join("<span class='separator'>|</span>")
   end
 
   def remove_diagnosis
-
+    uri = "http://www.who.int/feeds/entity/csr/don/en/rss.xml"
+    output = RestClient.get(uri)
+    doc = Nokogiri::XML(output)
+    @xml = doc.xpath("//channel/item").map do |i|
+      {
+        'title' => i.xpath('title').text, 'description' => i.xpath('description').text
+      }
+    end
+    @headlines = @xml.map{|x|x["title"]}.join("<span class='separator'>|</span>")
   end
 
   def view_diagnoses
-    
+    uri = "http://www.who.int/feeds/entity/csr/don/en/rss.xml"
+    output = RestClient.get(uri)
+    doc = Nokogiri::XML(output)
+    @xml = doc.xpath("//channel/item").map do |i|
+      {
+        'title' => i.xpath('title').text, 'description' => i.xpath('description').text
+      }
+    end
+    @headlines = @xml.map{|x|x["title"]}.join("<span class='separator'>|</span>")
   end
 end
